49c0a1b2efdf0907dbacf1eacaf01cf41cfffd66
mcn <root@nakas.dev>
2024-10-16 20:23:07 +0300

    added gen plugin

---
+++ lua/mcn/lazy/gen.lua
@@ -0,0 +1,9 @@
+return {
+    "David-Kunz/gen.nvim",
+    opts = {
+        model = "qwen2.5-coder:7b",
+        display_mode = "split",
+        show_prompt = true,
+        no_auto_close = true,
+    }
+}
