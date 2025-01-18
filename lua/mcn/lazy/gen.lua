0eb67aa49800f08ff0937e4d83513f78afda16a1
mcn <root@nakas.dev>
2025-01-18 11:03:04 +0100

    remove unused plugins

--- lua/mcn/lazy/gen.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "David-Kunz/gen.nvim",
-    opts = {
-        model = "qwen2.5-coder:7b",
-        display_mode = "split",
-        show_prompt = true,
-        no_auto_close = true,
-    }
-}
