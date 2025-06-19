3d317da3d0242eaeeb9c3a61e050d12d25a38999
mcn <root@nakas.dev>
2025-06-19 17:34:02 +0300

    feat: node setup

--- lua/mcn/defaults.lua
+++ lua/mcn/defaults.lua
@@ -1,3 +1,5 @@
+vim.env.PATH = "/Users/mcn/.nvm/versions/node/v24.2.0/bin:" .. (vim.env.PATH or "")
+
 vim.api.nvim_create_autocmd("BufReadPost", {
 	callback = function()
 		local last_line = vim.fn.line("'\"")
--- lua/mcn/lazy/conform.lua
+++ lua/mcn/lazy/conform.lua
@@ -3,28 +3,6 @@ return {
 	opts = {},
 
 	config = function()
-		local function get_node_path()
-			local handle = io.popen("source ~/.nvm/nvm.sh && which node 2>/dev/null")
-			if handle then
-				local result = handle:read("*a")
-				handle:close()
-				if result and result ~= "" then
-					return result:gsub("\n", ""):match("(.+)/[^/]+$")
-				end
-			end
-
-			vim.notify("Could not find node path", vim.log.levels.ERROR)
-
-			return nil
-		end
-
-		local node_path = get_node_path()
-
-		if not node_path then
-			vim.notify("Skipping prettier setup due to missing Node.js", vim.log.levels.WARN)
-			return
-		end
-
 		require("conform").setup({
 			formatters_by_ft = {
 				go = { "gofumpt", "goimports" },
@@ -40,14 +18,6 @@ return {
 
 				yaml = { "prettier" },
 				markdown = { "prettier" },
-			},
-
-			formatters = {
-				prettier = {
-					env = {
-						PATH = node_path .. ":" .. (vim.env.PATH or ""),
-					},
-				},
 			},
 
 			format_on_save = {
